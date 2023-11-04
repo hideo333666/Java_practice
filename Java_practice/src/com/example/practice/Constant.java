9bb4ff0222ce958282bdb81936704089f8179369
hideo333666 <muramura031409@gmail.com>
2023-11-04 23:34:33 +0900

    定数の宣言

--- Java_practice/src/com/example/practice/Constant.java
+++ Java_practice/src/com/example/practice/Constant.java
@@ -6,5 +6,16 @@ public class Constant {
 		System.out.println("私の年齢は" + age);
 		age = 31; 
 		System.out.println("いや、本当の年齢は" + age);
+		
+        final double PI = 3.14;
+        int pie = 5;
+        System.out.println("半径" + pie + "cmのパイの面積は、");
+        System.out.println(pie * pie * PI);
+        System.out.println("パイの半径を倍にします");
+        PI = 10;
+        System.out.println("半径" + pie + "cmのパイの面積は、");
+        System.out.println(pie * pie * PI);
+ 
+		
 	}
 }
