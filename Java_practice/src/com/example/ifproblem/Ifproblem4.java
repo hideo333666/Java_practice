f980fa63b33f13cbd0cd6e28c0359cc498285b09
hideo333666 <muramura031409@gmail.com>
2023-11-16 23:20:42 +0900

    if文

--- Java_practice/src/com/example/ifproblem/Ifproblem4.java
+++ Java_practice/src/com/example/ifproblem/Ifproblem4.java
@@ -2,11 +2,16 @@ package com.example.ifproblem;
 
 public class Ifproblem4 {
 	public static void main(String[] args) {
-		String signal = "red"; //ここに判定したい信号の色を入力
+		int score = 90;
 		
-		if(signal.equals("red")) {
-			
+		if (score >= 90) {
+			System.out.println("優秀");
+		} else if (score >= 70) {
+			System.out.println("良好");
+		} else if (score >= 50) {
+			System.out.println("普通");
+		} else {
+			System.out.println("不合格");
 		}
-		
 	}
 }
