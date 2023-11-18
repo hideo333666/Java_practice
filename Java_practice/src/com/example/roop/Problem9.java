d7eb99f3966d1b61a365435e28bc453f5f862422
hideo333666 <muramura031409@gmail.com>
2023-11-18 23:33:11 +0900

    while文基礎

---
+++ Java_practice/src/com/example/roop/Problem9.java
@@ -0,0 +1,12 @@
+package com.example.roop;
+
+public class Problem9 {
+	public static void main(String[] args) {
+		int i = 1; //初期化
+		
+		while (i <= 5) {
+			System.out.println(i); // 現在の数字を出力
+			i++; //数字を1増やす
+		}
+	}
+}
