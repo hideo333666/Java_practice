88e70bbd6d9e018424ccb58231ee95e94c60dfe9
hideo333666 <muramura031409@gmail.com>
2023-11-19 21:18:33 +0900

    java配列とループ

--- Java_practice/src/com/example/array/Problem05.java
+++ Java_practice/src/com/example/array/Problem05.java
@@ -1,5 +1,22 @@
 package com.example.array;
 
 public class Problem05 {
-
+	public static void main(String[] args) {
+		int[] numbers = {1, 3, 5, 7, 9};
+		int target = 11; //検索する値
+		boolean found = false;
+		
+		for (int num : numbers) {
+			if(num == target) {
+				found = true;
+				break;
+			}
+		}
+		
+		if (found) {
+			System.out.println(target + " は配列に存在します、");
+		} else {
+			System.out.println(target + " は配列に存在しません。");
+		}
+	}
 }
