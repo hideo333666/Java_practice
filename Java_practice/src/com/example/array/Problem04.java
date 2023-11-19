9d3887389694eec7e4f7520db143f8c8bb70d81c
hideo333666 <muramura031409@gmail.com>
2023-11-19 21:12:31 +0900

    java配列とループ

--- Java_practice/src/com/example/array/Problem04.java
+++ Java_practice/src/com/example/array/Problem04.java
@@ -1,6 +1,17 @@
 package com.example.array;
 
 public class Problem04 {
-	
-
+	public static void main(String[] args) {
+		int[] numbers = {5, 4, 3, 2, 1};
+		int[] reversed = new int[numbers.length];
+		
+		for (int i = 0; i < numbers.length; i++) {
+			reversed[i] = numbers[numbers.length - 1 - i];
+		}
+		
+		//結果を出力（オプション）
+		for (int num : reversed) {
+			System.out.print(num + " ");
+		}
+	}
 }
